70776fa9b627dcc285b0e79d900cc6d052f9d585
John Hunt <john.hunt10@gmail.com>
2020-10-29 11:26:43 +0000

    Added examples for functions

---
+++ 06-functions/function1.py
@@ -0,0 +1,18 @@
+# Examples of simple functions in Python
+
+def print_msg():
+    print('Hello World!')
+
+
+print_msg()
+print(type(print_msg))
+
+
+def print_my_msg(msg):
+    print(msg)
+
+
+print_my_msg('Hello World')
+print_my_msg('Good day')
+print_my_msg('Welcome')
+print_my_msg('Ola')

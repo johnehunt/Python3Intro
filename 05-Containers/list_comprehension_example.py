db9d33b8704df7fe85e4f8bfb56809ce83eff63a
John Hunt <john.hunt10@gmail.com>
2020-10-29 11:17:51 +0000

    Added example of list comprehension with a guard

--- 05-Containers/list_comprehension_example.py
+++ 05-Containers/list_comprehension_example.py
@@ -4,3 +4,6 @@ print('values', values)
 # List comprehension example
 new_values = [i + 1 for i in values]
 print('new_values', new_values)
+
+list = [item + 1 for item in values if item % 2 == 0]
+print('list:', list)

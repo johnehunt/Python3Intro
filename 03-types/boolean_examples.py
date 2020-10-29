cfc89aba64aeb893f8c04093d639eaf7e56f843a
John Hunt <john.hunt10@gmail.com>
2020-10-29 10:49:28 +0000

    Example of using bool

---
+++ 03-types/boolean_examples.py
@@ -0,0 +1,16 @@
+# Examplesof working with booleans
+
+all_ok = True
+print(all_ok)
+all_ok = False
+print(all_ok)
+print(type(all_ok))
+
+print(int(True))
+print(int(False))
+print(bool(1))
+print(bool(0))
+
+status = bool(input('OK to proceed: '))
+print(status)
+print(type(status))

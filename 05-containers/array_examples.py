3ac3da62aea3c408860866e035607212aa9501ab
John Hunt <john.hunt10@gmail.com>
2020-11-04 15:52:20 +0000

    Added example of using Python arrays as containers

---
+++ 05-containers/array_examples.py
@@ -0,0 +1,46 @@
+# Provides examples of using the Python array module
+# to hold an ordered, mutable, indexed sequence of values
+# Note in many examples the term list and array is used
+# interchangeably in Python, but Python arrays are not lists
+# They are less flexible but are more space efficient and can be faster
+
+# Import the Python array module
+import array as arr
+
+# Create an array of numeric values
+# created an array of float type.
+# The letter d is a type code which determines
+# the type of the array during creation
+print('Creating an array of doubles')
+data = arr.array('d', [1.1, 3.5, 4.5])
+print('data:', data)
+
+# Creating an array of integers
+data = arr.array('i', [1, 2, 5, 6, 9])
+print('data:', data)
+
+# Use indices to access elements of array
+# Note index starts from 0 (not 1)
+print("First element:", data[0])
+print("Second element:", data[1])
+print("Last element:", data[-1])
+
+# Can obtain a sub array
+print('data[2:4]:', data[2:4])  # 3rd to 4th
+
+# Change a value at an index
+# changing first element
+data[2] = 0
+print('Updated data:', data)
+
+# Can remove array elements
+del data[2]  # removing third element
+print('Data after removal:', data)
+
+# Can concatenate arrays
+odd = arr.array('i', [1, 3, 5])
+even = arr.array('i', [2, 4, 6])
+# Concatenate the two arrays together
+numbers = odd + even
+
+print(numbers)
